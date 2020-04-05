f20505f933f5e9f129ac4e33b5eafc7fb63c8d87
David E. Weirich <weirich.david@gmail.com>
2020-04-05 12:54:18 -0400

    Comments

--- battle.py
+++ battle.py
@@ -8,7 +8,7 @@ from pokemon.pokemon import Pokemon, moves
 
 
 # List of all the current pokemon types in the game
-pokemon = json.load(open("pokemon_types.json"))
+pokemon_types = json.load(open("pokemon_types.json"))
 
 
 def print_scene(p0, p1):
@@ -18,8 +18,8 @@ def print_scene(p0, p1):
 
 if __name__ == "__main__":
     # The two pokemon battling
-    pokemon0 = Pokemon(pokemon[0], 5)
-    pokemon1 = Pokemon(pokemon[1], 5)
+    pokemon0 = Pokemon(pokemon_types[0], level=5)
+    pokemon1 = Pokemon(pokemon_types[1], level=5)
 
     # Keep battling until one pokemon faints
     while pokemon0.hp > 0 and pokemon1.hp > 0:
@@ -33,12 +33,14 @@ if __name__ == "__main__":
         move1 = moves[pokemon1.moves[choice1]]
 
         # Use the moves
+        # TODO: Choose who goes first based on speed stat
         print(f"{pokemon0.name} used {move0}!")
         move0(pokemon0, pokemon1)
 
         print(f"{pokemon1.name} used {move1}!")
         move1(pokemon1, pokemon0)
 
+        # TODO: Don't let both pokemon faint on the same turn (unless self destruct is used I guess...)
         if pokemon0.hp <= 0:
             print(f"{pokemon0.name} fainted!")
         if pokemon1.hp <= 0:
